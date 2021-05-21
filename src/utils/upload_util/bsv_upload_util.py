1b2e4a0f53f31a8bafa3b34d27ef008d664a1e12
tys-hiroshi <tashiro.hiroshi@toyoko-sys.co.jp>
2021-05-21 13:00:36 +0900

    add

--- src/utils/upload_util/bsv_upload_util.py
+++ src/utils/upload_util/bsv_upload_util.py
@@ -5,4 +5,12 @@ class BsvUploadUtil():
         self.uploader = polyglot.Upload(private_key_wif, "test")
 
     def upload(self, file: bytes, media_type: str = None, encoding: str =None, file_name: str = None):
-        self.uploader.upload_bcat(file, media_type, encoding, file_name)
+        #self.uploader.upload_bcat(file, media_type, encoding, file_name)
+        #self.uploader.bcat_parts_send_from_binary(file)
+        media_type = self.uploader.get_media_type_for_file_name(file_name)
+        encoding = self.uploader.get_encoding_for_file_name(file_name)
+        print(media_type)
+        print(encoding)
+        #rawtx = self.uploader.b_create_rawtx_from_binary(file, media_type, encoding, file_name)  ## < 100kb
+        rawtx = self.uploader.bcat_parts_send_from_binary(file)  ## < 100kb
+        txid = self.uploader.send_rawtx(rawtx)

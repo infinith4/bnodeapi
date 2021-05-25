e85f6c23607b53fd1555ea971177562c04d1afbd
tys-hiroshi <tashiro.hiroshi@toyoko-sys.co.jp>
2021-05-25 13:00:26 +0900

    mod

--- src/test_bcat_download/bcat_download.py
+++ src/test_bcat_download/bcat_download.py
@@ -7,12 +7,30 @@ from polyglot.upload import Upload
 import gzip
 
 from bitsv.network import NetworkAPI
+# [PREFIXES]
+B = '19HxigV4QyBv3tHpQVcUEQyq1pzZVdoAut'  # https://b.bitdb.network/
+C = '19HxigV4QyBv3tHpQVcUEQyq1pzZVdoAut'  # https://c.bitdb.network/ --> writing to blockchain is same as B://
+BCAT = '15DHFxWZJT58f9nhyGnsRBqrgwK4W6h4Up'  # https://bcat.bico.media/
+BCATPART = '1ChDHzdd1H4wSjgGMHyndZm6qxEDGjqpJL'  # https://bcat.bico.media/ (raw data only after prefix)
+D = '19iG3WTYSsbyos3uJ733yK4zEioi1FesNU'  # Dynamic - ownership over state of address
+AIP = '15PciHG22SNLQJXMoSUaWVi7WSqc7hCfva'  # https://github.com/BitcoinFiles/AUTHOR_IDENTITY_PROTOCOL
+MAP = '1PuQa7K62MiKCtssSLKy1kh56WWU7MtUR5'  # MAP protocol.
 
 class Download(NetworkAPI):
     @staticmethod
     def do_download(txid : str) -> dict:
         dl = Download("test")
         #data_dict : dict = dl.bcat_fields_from_txid(txid)
+        scripts = dl.scripts_from_txid(txid)
+        #print(scripts)
+        data = dl.pushdata_from_script(scripts[0])
+        print(data)
+        fields = dl.bcat_linker_fields_from_pushdata(data)
+        print(fields)
+        part_binary = dl.bcat_part_binary_from_pushdata(data)
+        print(part_binary)
+        fields = dl.bcat_linker_fields_from_txid(txid)
+        print(fields)
         data_dict = dl.download_bcat(txid, "test")
         return data_dict
 
